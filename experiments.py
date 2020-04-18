73b97c479510896128f4b6d504ff6a5cad859e69
kur <rhdnfka94@gmail.com>
2020-04-18 12:17:23 +0900

    Set main args and Print expr result in py

--- experiments.py
+++ experiments.py
@@ -7,3 +7,22 @@ insert/remove from back
 
 insert/remove random position
 '''
+
+import subprocess as sp
+import sys
+
+import yaml
+import matplotlib.pyplot as plt
+import funcy as F
+from tqdm import tqdm
+
+def expr_result(num_data, node_size):
+    ''' experiment result in milli second '''
+    return yaml.safe_load(
+        sp.check_output(
+            #['./main', str(int(num_data)), str(int(node_size))],
+            ['./main', str(num_data), str(node_size)],
+            universal_newlines=True))
+
+expr = expr_result(1e3, 50)
+print(expr)
--- main.cpp
+++ main.cpp
@@ -22,10 +22,23 @@ namespace crn = std::chrono;
 
 
 int main(int argc, char* argv[]){
-    int num_data = 1e3;
-    int node_size = 64;
+    //int num_data = 1e6; int node_size = 1e3;
+    int num_data = 1e3; int node_size = 50;
+    //int num_data = 20; int node_size = 10;
+    
+    if(argc > 1){
+        int argv1ret = sscanf(argv[1], "%d", &num_data);
+        int argv2ret = sscanf(argv[2], "%d", &node_size);
+        if(argv1ret == EOF || argv2ret == EOF){
+            std::cout << "argv error: " << '\n';
+            std::cout << argv[0] << ',' 
+                      << argv[1] << ',' 
+                      << argv[2] << '\n';
+        }
+    }
 
     std::cout << "data.num: " << num_data << "\n";
+    std::cout << "node.size: " << node_size << "\n";
     ULNode* ulist; init_list(&ulist, node_size); //unrolled
     ULNode* list;  init_list(&list, 1); // just linked list
 
@@ -34,20 +47,48 @@ int main(int argc, char* argv[]){
             insert(&ulist, 0, i);
         }
     );
+    //print_list(ulist, TRUE);
     EXPR("ul.remove.front", "ulist remove front running time",
         for(int i = 0; i < num_data; i++){
             remove(&ulist, 0, NULL);
         }
     );
 
+
     EXPR("l.insert.front", "list insert front running time",
         for(int i = 0; i < num_data; i++){
             insert(&list, 0, i);
         }
     );
+    //print_list(list, TRUE);
     EXPR("l.remove.front", "list remove front running time",
         for(int i = 0; i < num_data; i++){
+            //std::cout << i << ", ";
             remove(&list, 0, NULL);
+        }
+    );
+
+
+    EXPR("ul.insert.back", "ulist insert back running time",
+        for(int i = 0; i < num_data; i++){
+            insert(&ulist, -1, i);
+        }
+    );
+    EXPR("ul.remove.back", "ulist remove back running time",
+        for(int i = 0; i < num_data; i++){
+            remove(&ulist, -1, NULL);
+        }
+    );
+
+    EXPR("l.insert.back", "list insert back running time",
+        for(int i = 0; i < num_data; i++){
+            insert(&list, -1, i);
+        }
+    );
+    EXPR("l.remove.back", "list remove back running time",
+        for(int i = 0; i < num_data; i++){
+            //std::cout << i << ", ";
+            remove(&list, -1, NULL);
         }
     );
 }
